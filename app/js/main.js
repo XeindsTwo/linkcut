6f243de8c7a1f4c438cd8fabba2f0fab12588d8b
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2025-01-17 22:21:26 +0300

    add new sections, and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,29 @@
+import {setupMobileMenu} from "./mobileMenu.js";
+
+setupMobileMenu();
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = event.target.getAttribute('href').slice(1);
+  const targetElement = document.getElementById(targetId);
+
+  let targetOffset;
+
+  if (targetId === 'plan') {
+    targetOffset = targetElement.offsetTop - 10;
+  } else {
+    targetOffset = targetElement.offsetTop - 30;
+  }
+
+  window.scrollTo({top: targetOffset, behavior: 'smooth'});
+}
+
+const menuLinks = document.querySelectorAll('.desktop');
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', scrollToSection);
+});
+
 const swiper = new Swiper('.team__swiper', {
   slidesPerView: 3,
   spaceBetween: 20,
